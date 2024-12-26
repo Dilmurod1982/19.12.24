46cb3f720a301c3840f4da828bba2f264e026785
cipher <dilsurbet@hmail.com>
2024-12-26 23:16:26 +0500

    1234234

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Docs() {
-  return <div>Docs</div>;
+  return (
+    <div>
+      <h1>Озгина кутинг!</h1>
+      <h2>Сахифа ишлаб чиқилмоқда</h2>
+    </div>
+  );
 }
 
 export default Docs;
